e22603c096b37a1bcf801b6ffe08846d2af0a650
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-01-06 17:31:39 -0500

    Update

---
+++ MainHerencia.java
@@ -0,0 +1,14 @@
+
+package HerenciaJava;
+
+    /**
+     * @autor: jlianacastillo
+     */   
+public class main {
+   public static void main (String [] args){
+       LecheChocolate objeto = new LecheChocolate ();
+       LecheFresa objetoFresa = new LecheFresa ();
+       objeto.imprimirLecheChocolate();
+       objetoFresa.imprimirLecheFresa();
+   }
+}
